a9ab38decc5cf0e2b5d87f2b8928c11cd71b6424
Jan Vollmer <jan@vllmr.dev>
2024-12-09 20:31:05 +0100

    chore(ci): limit push actions to dev branch and implement simple bundler test

---
+++ tests/test_bundle/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from pathlib import Path
+import typing as t
+from flay.bundle.package import bundle_package
+import sys
+import shutil
+
+RunBundlePackageT = t.Callable[[str, str], tuple[Path, Path]]
+
+
+PACKAGES_PATH = Path() / "tests" / "test_bundle" / "packages"
+
+
+@pytest.fixture
+def run_bundle_package(tmp_path: Path) -> RunBundlePackageT:
+    def _run_bundle_package(
+        package_name: str, module_spec: str, vendor_module_name: str = "_vendor"
+    ) -> tuple[Path, Path]:
+        pre_bundle_path = tmp_path / "pre_bundle"
+        bundled_path = tmp_path / "bundled"
+        shutil.copytree(
+            str(PACKAGES_PATH / package_name), str(pre_bundle_path / package_name)
+        )
+        sys.path = [str(pre_bundle_path), *sys.path]
+        try:
+            bundle_package(
+                module_spec=module_spec,
+                destination_path=bundled_path,
+                vendor_module_name=vendor_module_name,
+            )
+        finally:
+            sys.path = sys.path[1:]
+        return pre_bundle_path / package_name, bundled_path / package_name
+
+    return _run_bundle_package
---
+++ tests/test_bundle/test_bundle_package.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+import typing as t
+import os
+
+if t.TYPE_CHECKING:
+    from .conftest import RunBundlePackageT
+
+
+def test_simple_bundle_hello_world(run_bundle_package: RunBundlePackageT) -> None:
+    source_path, result_path = run_bundle_package("hello_world", "hello_world")
+
+    for (sub_path1, dirs1, files1), (sub_path2, dirs2, files2) in zip(
+        os.walk(str(source_path)), os.walk(str(result_path))
+    ):
+        assert dirs2 == dirs1
+        assert files2 == files1
+        for file1, file2 in zip(files1, files2):
+            assert file2 == file1
+            with open(sub_path1 + "/" + file1) as f:
+                content1 = f.read()
+
+            with open(sub_path2 + "/" + file2) as f:
+                content2 = f.read()
+
+            assert content2 == content1
