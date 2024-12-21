9f025099bde9f1b904389e0d68831e403907171c
Jan Vollmer <jan@vllmr.dev>
2024-12-21 15:29:14 +0100

    implement Fold

--- rust/src/bundle/imports_transformer.rs
+++ rust/src/bundle/imports_transformer.rs
@@ -1,10 +1,9 @@
-use std::collections::HashSet;
-
 use pyo3::pyfunction;
 use rustpython_ast::{
-    text_size::TextRange, Identifier, StmtImport, StmtImportFrom, Suite, Visitor,
+    text_size::TextRange, Alias, Fold, Identifier, Stmt, StmtImport, StmtImportFrom, Suite,
 };
-use rustpython_parser::Parse;
+use rustpython_parser::{Parse, ParseError};
+use std::collections::HashSet;
 
 use crate::common::module_spec::{get_top_level_package, is_in_std_lib};
 
@@ -46,28 +45,68 @@ impl ImportsTransformer {
         return self._prepend_vendor(&node).to_owned();
     }
 }
-// implement Fold instead
-impl Visitor for ImportsTransformer {
-    fn visit_stmt_import(&mut self, mut node: StmtImport<TextRange>) {
-        node.names.iter_mut().for_each(|name| {
-            name.name = self._prepend_vendor_import(
-                name.name.to_owned(),
-                name.name.as_str(),
-                name.asname.is_none(),
-            )
-        });
+
+impl Fold<TextRange> for ImportsTransformer {
+    fn fold_stmt_import(
+        &mut self,
+        node: StmtImport<TextRange>,
+    ) -> Result<StmtImport<TextRange>, ParseError> {
+        Ok(StmtImport {
+            names: node
+                .names
+                .iter()
+                .map(|name| Alias {
+                    range: name.range,
+                    name: self._prepend_vendor_import(
+                        name.name.to_owned(),
+                        name.name.as_str(),
+                        name.asname.is_none(),
+                    ),
+                    asname: name.asname.to_owned(),
+                })
+                .collect(),
+            range: node.range,
+        })
     }
 
-    fn visit_stmt_import_from(&mut self, mut node: StmtImportFrom<TextRange>) {
+    fn fold_stmt_import_from(
+        &mut self,
+        node: StmtImportFrom<TextRange>,
+    ) -> Result<StmtImportFrom<TextRange>, ParseError> {
         if node.module.is_some() && node.level.is_none() {
             let module_node = node.module.unwrap();
             let module_spec = module_node.as_str();
-            node.module = Option::from(self._prepend_vendor_import(
+            let new_module = Option::from(self._prepend_vendor_import(
                 module_node.to_owned(),
                 module_spec,
                 false,
             ));
+            return Ok(StmtImportFrom {
+                level: node.level,
+                names: node.names,
+                range: node.range,
+                module: new_module,
+            });
         }
+        Ok(node)
+    }
+
+    type TargetU = TextRange;
+
+    type Error = ParseError;
+
+    type UserContext = bool;
+
+    fn will_map_user(&mut self, _user: &TextRange) -> Self::UserContext {
+        return false;
+    }
+
+    fn map_user(
+        &mut self,
+        user: TextRange,
+        _context: Self::UserContext,
+    ) -> Result<Self::TargetU, Self::Error> {
+        Ok(user)
     }
 }
 #[pyfunction]
@@ -77,12 +116,15 @@ pub fn transform_imports(
     top_level_package: &str,
     vendor_module_name: &str,
 ) {
-    let mut asts = Suite::parse(source, source_path).unwrap();
-    asts.iter_mut().for_each(|ast| {
-        let transformer = ImportsTransformer::new(
+    let asts = Suite::parse(source, source_path).unwrap();
+    let mut new_asts: Vec<Stmt> = Vec::new();
+    // let unparser = Unparser { f: ??? };
+    asts.iter().for_each(|ast| {
+        let mut transformer = ImportsTransformer::new(
             top_level_package.to_string(),
             vendor_module_name.to_string(),
         );
-        // transformer.visit_stmt(ast);
+        let new_ast = transformer.fold_stmt(ast.to_owned());
+        new_asts.push(new_ast.unwrap());
     });
 }
