7a5289273a099091117452cd2ab3841e541c560d
Jan Vollmer <jan@vllmr.dev>
2025-01-13 21:19:53 +0100

    more progress

--- rust/src/treeshake/references_counter.rs
+++ rust/src/treeshake/references_counter.rs
@@ -1,9 +1,12 @@
 use std::{collections::HashMap, path::PathBuf};
 
 use pyo3::{pyclass, pymethods};
-use rustpython_ast::{Expr, Stmt, StmtImport, StmtImportFrom, Visitor};
+use rustpython_ast::{Expr, ExprCompare, Stmt, StmtImport, StmtImportFrom, Visitor};
 
-use crate::providers::fully_qualified_name_provider::FullyQualifiedNameProvider;
+use crate::{
+    common::{ast::get_import_from_absolute_module_spec, module_spec::get_parent_package},
+    providers::fully_qualified_name_provider::FullyQualifiedNameProvider,
+};
 
 #[pyclass]
 pub struct ReferencesCounter {
@@ -89,14 +92,80 @@ impl ReferencesCounter {
     }
 }
 
+fn is_if_name_main(expr: &Expr) -> bool {
+    if let Expr::Compare(ExprCompare {
+        ops: cmp_ops,
+        left,
+        comparators,
+        ..
+    }) = expr
+    {
+        // NOTE: make more readable?
+        return cmp_ops.len() == 1
+            && cmp_ops[0].is_eq()
+            && comparators.len() == 1
+            && ((left
+                .as_name_expr()
+                .is_some_and(|name| name.id.as_str() == "__name__")
+                && comparators[0].as_constant_expr().is_some_and(|c| {
+                    c.value
+                        .as_str()
+                        .is_some_and(|c_value| c_value == "__main__")
+                }))
+                || (comparators[0]
+                    .as_name_expr()
+                    .is_some_and(|name| name.id.as_str() == "__name__")
+                    && left.as_constant_expr().is_some_and(|c| {
+                        c.value
+                            .as_str()
+                            .is_some_and(|c_value| c_value == "__main__")
+                    })));
+    }
+
+    return false;
+}
+
 impl Visitor for ReferencesCounter {
     fn visit_stmt(&mut self, stmt: Stmt) {
         if self.always_bump {
             self.maybe_increase_stmt(&stmt);
         }
+        let mut next_always_bump = false;
+
+        match &stmt {
+            Stmt::ClassDef(class_def) => {
+                if class_def.decorator_list.len() > 0 || self.has_references_for_stmt(&stmt) {
+                    self.maybe_increase_stmt(&stmt);
+                    next_always_bump = true;
+                }
+            }
+            Stmt::AnnAssign(_) | Stmt::Assign(_) => {
+                if self.has_references_for_stmt(&stmt) {
+                    self.maybe_increase_stmt(&stmt);
+                    next_always_bump = true;
+                }
+            }
+            Stmt::If(if_block) => {
+                if is_if_name_main(&if_block.test) {
+                    self.maybe_increase_stmt(&stmt);
+                    next_always_bump = true;
+                }
+            }
+            Stmt::ImportFrom(import_from) => {
+                if import_from.names.len() == 1 && import_from.names[0].name.as_str() == "*" {
+                    while let Ok(module_spec) = get_import_from_absolute_module_spec(
+                        &import_from,
+                        &get_parent_package(&self.module_spec),
+                    ) {}
+                }
+            }
+            _ => {}
+        }
 
         let scope = self.names_provider.enter_scope(&stmt);
+        self.always_bump = next_always_bump;
         self.generic_visit_stmt(stmt);
+        self.always_bump = false;
         self.names_provider.exit_scope(scope);
     }
 
