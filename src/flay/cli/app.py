f0aada0d18bb71fb9e136038f2808e08ae76df1e
Jan Vollmer <jan@vllmr.dev>
2025-04-21 19:24:34 +0200

    feat(cli): create initial CLI for general usage

--- src/flay/cli/app.py
+++ src/flay/cli/app.py
@@ -1,17 +1,69 @@
+from flay.bundle import DEFAULT_BUNDLE_METADATA, DEFAULT_VENDOR_MODULE_NAME
 from flay.common.logging import enable_debug_logging
 from .debug import DebugApp
 from flay.common.pydantic import FlayBaseSettings
-from pydantic_settings import CliApp, CliSubCommand
+from pydantic_settings import CliApp, CliSubCommand, SettingsError, CliPositionalArg
 from .options import DebugFlagT
 import os
+from pydantic import Field, AliasChoices
+from pathlib import Path
+import typing as t
+from .bundle import cli_bundle_package
+from .treeshake import cli_treeshake_package
+from flay.common.rich import console, check
 
 
 class Flay(FlayBaseSettings):
+    module_spec: CliPositionalArg[
+        t.Annotated[str, Field(description="Module that should be bundled")]
+    ]
+    output_path: t.Annotated[
+        Path,
+        Field(
+            description="Target path for the generated bundle",
+            validation_alias=AliasChoices("output", "o"),
+        ),
+    ] = Path("flayed")
+    vendor_module_name: t.Annotated[
+        str, Field(description="Name of the module where external packages should be")
+    ] = DEFAULT_VENDOR_MODULE_NAME
+    bundle_metadata: t.Annotated[
+        bool,
+        Field(
+            description="Whether package metadata should be collocated with the generated bundle"
+        ),
+    ] = DEFAULT_BUNDLE_METADATA
+
+    treeshake: t.Annotated[
+        bool,
+        Field(
+            description="Should unused source code be stripped from the bundle?",
+            validation_alias=AliasChoices("t"),
+        ),
+    ] = True
+
     debug: DebugFlagT = False
     if os.getenv("FLAY_DEBUG_APP"):
         debug_app: CliSubCommand[DebugApp]
 
+    def main(self) -> None:
+        console.print(f"Starting to bundle module {self.module_spec}...")
+        cli_bundle_package(
+            self.module_spec,
+            self.output_path,
+            self.vendor_module_name,
+            self.bundle_metadata,
+        )
+        console.print(check, f"Finished bundling {self.module_spec}")
+        if self.treeshake:
+            console.print("Start removing unused code...")
+            cli_treeshake_package(str(self.output_path.absolute()))
+            console.print(check, "Finished removing unused code")
+
     def cli_cmd(self) -> None:
         if self.debug:
             enable_debug_logging()  # pragma: no cover
-        CliApp.run_subcommand(self)
+        try:
+            CliApp.run_subcommand(self, False)
+        except SettingsError:
+            self.main()
---
+++ src/flay/cli/bundle.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+from pathlib import Path
+from rich.progress import SpinnerColumn, TextColumn, MofNCompleteColumn
+from flay.common.rich import console, check
+from flay.bundle.package import bundle_package
+import typing_extensions as te
+import typing as t
+from rich.progress import Progress, BarColumn
+
+
+class BundlePackageCliIO:
+    found_modules_counter: int
+    processed_modules: int
+    total_modules: int | None
+
+    def __init__(self, initial_module_spec: str) -> None:
+        self.progress = Progress(
+            SpinnerColumn(finished_text=check),
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            MofNCompleteColumn(),
+            TextColumn("[dim]({task.fields[spec]})"),
+            console=console,
+        )
+
+        self.find_modules_task = self.progress.add_task(
+            description="Searching for modules...", total=None, spec=initial_module_spec
+        )
+        self.process_modules_task = self.progress.add_task(
+            visible=False, description="Processing modules...", spec=initial_module_spec
+        )
+        self.total_modules = None
+        self.found_modules_counter = 0
+        self.processed_modules = 0
+
+    def on_found_module(self, spec: str) -> None:
+        self.found_modules_counter += 1
+        self.progress.update(
+            self.find_modules_task, spec=spec, completed=self.found_modules_counter
+        )
+
+    def on_found_total_modules(self, count: int) -> None:
+        self.total_modules = count
+        self.progress.update(self.find_modules_task, completed=count, total=count)
+
+    def on_process_module(self, spec: str) -> None:
+        self.processed_modules += 1
+        if self.processed_modules <= (self.total_modules or 0):
+            self.progress.update(
+                self.process_modules_task,
+                visible=True,
+                total=self.total_modules,
+                completed=self.processed_modules,
+                spec=spec,
+            )
+
+    def end_progress(self) -> None:
+        if self.progress.live._started:
+            self.progress.stop()
+
+    def on_bundled_metadata(self) -> None:
+        self.end_progress()
+        console.print(check, "Copied package metadata")
+
+    def __enter__(self) -> te.Self:
+        self.progress.start()
+        return self
+
+    def __exit__(self, *args: t.Any, **kw: t.Any) -> None:
+        self.end_progress()
+
+
+def cli_bundle_package(
+    module_spec: str, output_path: Path, vendor_module_name: str, bundle_metadata: bool
+) -> None:
+    with BundlePackageCliIO(initial_module_spec=module_spec) as io:
+        bundle_package(
+            module_spec,
+            output_path,
+            vendor_module_name=vendor_module_name,
+            bundle_metadata=bundle_metadata,
+            found_module_callback=io.on_found_module,
+            found_total_modules_callback=io.on_found_total_modules,
+            process_module_callback=io.on_process_module,
+            bundled_metadata_callback=io.on_bundled_metadata,
+        )
+
+
+__all__ = ["cli_bundle_package"]
