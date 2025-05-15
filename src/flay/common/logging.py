2c1d7ae20d1cd92e8a7e1bbb4f063ea73a12fde9
Jan Vollmer <jan@vllmr.dev>
2025-05-15 20:21:55 +0200

    fix(cli): logging does not break progress bars

--- src/flay/common/logging.py
+++ src/flay/common/logging.py
@@ -5,12 +5,13 @@ import platformdirs
 import contextvars
 import typing as t
 from contextlib import contextmanager
-
+from rich.logging import RichHandler
 import uuid
 from pathlib import Path
 import hashlib
 from rich.style import Style
 from flay.common.rich import ansi_style_text
+from .rich import console
 
 logfile_path_context: contextvars.ContextVar[str] = contextvars.ContextVar(
     "_flay_logfile"
@@ -35,25 +36,7 @@ LEVELNAME_FORMAT: dict[str, t.Callable[[str], str]] = {
 }
 
 
-class FlayFormatter(logging.Formatter):
-    def __init__(self, colored: bool = False) -> None:
-        self.colored = colored
-        super().__init__(fmt="%(asctime)s %(name)s %(levelname)s: %(message)s")
-
-    def formatTime(self, record: logging.LogRecord, datefmt: str | None = None) -> str:
-        formatted = super().formatTime(record, datefmt)
-        if self.colored:
-            formatted = ansi_style_text(formatted, Style(color="bright_magenta"))
-        return formatted
-
-    def format(self, record: logging.LogRecord) -> str:
-        if self.colored:
-            record.levelname = LEVELNAME_FORMAT[record.levelname](record.levelname)
-        return super().format(record)
-
-
-FORMATTER = FlayFormatter()
-COLORED_FORMATTER = FlayFormatter(colored=True)
+FORMATTER = logging.Formatter(fmt="%(asctime)s %(name)s %(levelname)s: %(message)s")
 
 
 def get_flay_logger() -> logging.Logger:
@@ -63,8 +46,7 @@ def get_flay_logger() -> logging.Logger:
 @contextmanager
 def setup_logger(command: str) -> t.Generator[None, t.Any, None]:
     flay_logger = get_flay_logger()
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(COLORED_FORMATTER)
+    stream_handler = RichHandler(console=console, log_time_format="[%Y-%m-%d %H:%M:%S]")
 
     logging_root_dir = platformdirs.user_log_dir("flay", ensure_exists=True)
     unique_id = hashlib.sha256(uuid.uuid4().bytes).hexdigest()[:8]
