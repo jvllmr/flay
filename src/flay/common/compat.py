44628a969f8db03a87e9ef30229e6f9a68d75015
Jan Vollmer <jan@vllmr.dev>
2025-05-23 21:43:01 +0200

    weird behavior on cpython@3.10

--- src/flay/common/compat.py
+++ src/flay/common/compat.py
@@ -1,7 +1,8 @@
 import sys
 
 
-if sys.version_info < (3, 10):
+if sys.version_info < (3, 11):
+    # packages_distributions is available, but does not work as expected with cpython@3.10 (rich was not discovered)
     from importlib_metadata import packages_distributions  # type: ignore[import-not-found,unused-ignore]
 else:
     from importlib.metadata import packages_distributions  # type: ignore[attr-defined]
