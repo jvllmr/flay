4eb3dd542e9c0a8943c20c21e938f9ab7e8a233d
Jan Vollmer <jan@vllmr.dev>
2025-08-26 21:34:43 +0200

    fix tests
    Signed-off-by: Jan Vollmer <jan@vllmr.dev>

--- tests/test_bundle/packages/annotation_string_literals/__init__.py
+++ tests/test_bundle/packages/annotation_string_literals/__init__.py
@@ -1,6 +1,6 @@
 import typer
 import typer as typerino
-
+import collections.abc
 random_literal = "typer.Typer"
 
 def modify_app(app: "typer.Typer") -> "typer.Typer":
@@ -8,3 +8,9 @@ def modify_app(app: "typer.Typer") -> "typer.Typer":
 
 def modify_app2(app2: "typerino.Typer") -> "typerino.Typer":
     return app2
+
+def accept_ordered_dict(ordered_dict: "collections.OrderedDict[str, str]") -> "collections.OrderedDict[str, str]":
+    return ordered_dict
+
+def accept_hashable(collection: "collections.abc.Hashable") -> "collections.abc.Hashable":
+    return collection
